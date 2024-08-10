b20bee01be4ed881cb7e3fba562daa5f495b90f2
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-08-10 18:42:09 -0300

    Styled WelcomeScreen

--- components/WelcomeScreen.jsx
+++ components/WelcomeScreen.jsx
@@ -1,25 +1,37 @@
 import { Link } from "expo-router";
 import { View, Text, Image, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Welcome = () => {
   return (
+    <SafeAreaView style={styles.container}>
+{/* COVERS HALF SCREEN */}
     <View>
       <Image source={require("../assets/BeerIcon.png")} />
-      <Text style={styles.title}>Séptima</Text>
-      <Text>Cocina Artesanal</Text>
-
+      <Text style={styles.logo_title}>Séptima</Text>
+      <Text style={styles.logo_subtitle}>Cocina Artesanal</Text>
+      </View>
+      {/* COVERS THE OTHER HALF */}
+      <View>
+<Text>¡Bienvenido!</Text>
+<Text>Séptima te acompaña en el elaborado de tu cerveza artesanal con recetas que podrían ayudarte a conseguir la textura y el sabor que estabas persiguiendo.</Text>
       <Link href="/login">Iniciar sesión</Link>
       <Link href="/register">Registrarse</Link>
       <Link href="/home">Continuar como invitado</Link>
     </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  title: {
+  logo_title: {
 fontWeight: "bold",
-fontSize: 30,
+fontSize: 40,
 fontFamily: "trainOne"
+  },
+  logo_subtitle: {
+    fontSize: 25,
+    fontFamily: "poppins"
   }
 })
 
